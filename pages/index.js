d0e851462b84afe8b94a036d21418c45c8c4d594
Vhko <hirsch-k@outlook.com>
2023-07-11 17:19:43 +0800

    add

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { getGlobalNotionData } from '@/lib/notion/getNotionData'
 import { setData } from '@/lib/notion/getPostBlocks'
 import Main from "./main"//引入一个页面
 import Image from 'next/image'
+import { document } from 'postcss'
 
 
 // @param {*} props
@@ -28,15 +29,10 @@ const Index = props => {
   return (
     <div className=''>
       <Main />
-      <div className='temp flex align-Center'>
-        <h2 className=''>{props.siteInfo.title}</h2>
-        {/* <ul>{listItems}</ul> */}
-        <div className='tags'>
-          <p>代码</p>
-        </div>
-        <div className='tags'>
-          <p>tags</p>
-        </div>
+      <canvas id="color-canvas"></canvas>
+      <div className='header flex align-Center target-element'>
+     
+        <h2 className='title'>{props.siteInfo.title}</h2>
       </div >
       <div className='div-margin flex align-Center'>
         <div className='h1-word-break flex'>
@@ -44,7 +40,7 @@ const Index = props => {
           <h1>{props.siteInfo.description}</h1>
         </div>
         <Image className="avatar"
-          // layout="fill"
+          //layout="fill"
           src={cover.imageUrl}
           alt={'Photo of ' + cover.name}
           width={1000}
@@ -55,38 +51,59 @@ const Index = props => {
   );
 
 }
+function getRandomImageColor(imageUrl) {
+  const canvas = document.getElementById('color-canvas');
+  const context = canvas.getContext('2d');
+  const image = new Image();
+  image.crossOrigin = 'Anonymous';
 
-/**
- * SSG 获取数据
- * @returns
- */
-export async function getStaticProps() {
-  const from = 'index'
-  const props = await getGlobalNotionData({ from })
+  image.onload = function() {
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.drawImage(image, 0, 0);
+    const pixelData = context.getImageData(0, 0, image.width, image.height).data;
+    const randomIndex = Math.floor(Math.random() * (pixelData.length / 4)) * 4;
+    const r = pixelData[randomIndex];
+    const g = pixelData[randomIndex + 1];
+    const b = pixelData[randomIndex + 2];
+    const randomColor = `rgb(${r}, ${g}, ${b})`;
+    document.getElementById('target-element').style.backgroundColor = randomColor;
+  };
 
-  const { siteInfo } = props
-  props.posts = props.allPages.filter(page => page.type === 'Post' && page.status === 'Published')
+  image.src = imageUrl;
+  getRandomImageColor(cover.imageUrl);
+}
+  /**
+   * SSG 获取数据
+   * @returns
+   */
+  export async function getStaticProps() {
+    const from = 'index'
+    const props = await getGlobalNotionData({ from })
 
-  // 预览文章内容
-  if (BLOG.POST_LIST_PREVIEW === 'true') {
-    for (const i in props.posts) {
-      const post = props.posts[i]
-      if (post.password && post.password !== '') {
-        continue
+    const { siteInfo } = props
+    props.posts = props.allPages.filter(page => page.type === 'Post' && page.status === 'Published')
+
+    // 预览文章内容
+    if (BLOG.POST_LIST_PREVIEW === 'true') {
+      for (const i in props.posts) {
+        const post = props.posts[i]
+        if (post.password && post.password !== '') {
+          continue
+        }
+        post.blockMap = await getPostBlocks(post.id, 'slug', BLOG.POST_PREVIEW_LINES)
       }
-      post.blockMap = await getPostBlocks(post.id, 'slug', BLOG.POST_PREVIEW_LINES)
+
+    }
+    // const prop = JSON.stringify(props);
+    setData(props)
+    return {
+      props: {
+        ...props
+      },
+      revalidate: parseInt(BLOG.NEXT_REVALIDATE_SECOND)
     }
 
   }
-  // const prop = JSON.stringify(props);
-  setData(props)
-  return {
-    props: {
-      ...props
-    },
-    revalidate: parseInt(BLOG.NEXT_REVALIDATE_SECOND)
-  }
 
-}
-
-export default Index
+  export default Index
