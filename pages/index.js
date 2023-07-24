1d36fa7c029c4726118bc1800862d3ee4bd15753
Vhko <hirsch-k@outlook.com>
2023-07-24 17:13:32 +0800

    🙌新增“章节”

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import BLOG from "@/blog.config";
-import { getPostBlocks } from "@/lib/notion";
-import { getGlobalNotionData } from "@/lib/notion/getNotionData";
-import { setData } from "@/lib/notion/getPostBlocks";
+import {getPostBlocks} from "@/lib/notion";
+import {getGlobalNotionData} from "@/lib/notion/getNotionData";
+import {setData} from "@/lib/notion/getPostBlocks";
 import Main from "./main"; //引入一个页面
 import Image from "next/image";
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
+import {Inter} from 'next/font/google'
+
+const inter = Inter({subsets: ['latin']})
 // import { document } from "postcss";
 // import { useEffect, useState } from 'react'
-import React, { useEffect, useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import anime from "animejs"
+import colors from "tailwindcss/colors";
 // import Test from "./main";
 
 //import anime from 'animejs/lib/anime.es.js';//动画库 https://animejs.com/documentation/#cssSelector
@@ -18,69 +20,122 @@ import anime from "animejs"
 
 const Index = (props) => {
   console.log(props)
-  const prop = JSON.stringify(props);
-  const listItems = props.posts.map((product) => (
-    <li key={product.id}>{product.title}</li>
-  ));
-  const cover = {
-    name: "图像",
-    imageUrl: props.siteInfo.pageCover,
-    imageSize: 100,
-  };
-
-
+  // const prop = JSON.stringify(props);
+  // const listItems = props.posts.map((product) => (
+  //     <li key={product.id}>{product.title}</li>
+  // ));
+  // const cover = {
+  //   name: "图像",
+  //   imageUrl: props.siteInfo.pageCover,
+  //   imageSize: 100,
+  // };
   // ------------------------------------------------------------------------//
   return (
-    <div className="">
-      <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
-        <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{props.siteInfo.title}</h1>
-      </div>
-      <div className="flex flex-wrap content-center justify-center w-full bg-white h-10 rounded-lg border-b border-gray-200">
-        <p className="text-sm">最新文章更新于{props.posts[0].createdTime}</p>
-      </div>
-      <div className="px-20 pb-16">
-        <section className=" w-full overflow-hidden grid grid-cols-1 grid-rows-2">
-          <div className="grid w-1/3 content-center">
-            <h1 className="text_vw_30 overflow-hidden text-clip font-medium ">{props.siteInfo.description}</h1>
-            <p className="flex justify-end flex-wrap pt-6 text_vw_15 text-left font-semibold">@{props.notice.Person[0].name} </p>
-          </div>
-          <div className="grid grid-cols-2 justify-items-end  content-center items-center h-full">
-            <div>
+      <div className="">
+        <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
+          <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{props.siteInfo.title}</h1>
+        </div>
+        <div
+            className="flex flex-wrap content-center justify-center w-full bg-white h-10 rounded-lg border-b border-gray-200">
+          <p className="text-sm">最新文章更新于{props.posts[0].createdTime}</p>
+        </div>
+        <div className="px-20 pb-16">
+          <section className=" w-full overflow-hidden grid grid-cols-1 grid-rows-2">
+            <div className="grid w-1/3 content-center">
+              <h1 className="text_vw_30 overflow-hidden text-clip font-medium ">{props.siteInfo.description}</h1>
+              <p className="flex justify-end flex-wrap pt-6 text_vw_15 text-left font-semibold">@{props.notice.Person[0].name} </p>
             </div>
-            <div className="pt--1">
+            <div className="grid grid-cols-2 justify-items-end  content-center items-center h-full">
+              <div>
+              </div>
+              <div className="pt--1">
+                <Image
+                    className="width-auto"
+                    src={props.siteInfo.pageCover}
+                    alt="PageCover"
+                    width={9999}
+                    height={9999}
+                    layout="cover"
+                    objectFit="contain"
+                />
+              </div>
+            </div>
+          </section>
+          <section className="grid grid-cols-2 gap-8">
+            <div className="w-11/12 h-1/2">
               <Image
-                className="width-auto"
-                src={props.siteInfo.pageCover}
-                alt="PageCover"
-                width={9999}
-                height={9999}
-                layout="cover"
-                objectFit="contain"
+                  src={props.notice.pageCover}
+                  alt="noticePageCover"
+                  width={9999}
+                  height={9999}
+                  layout="cover"
               />
             </div>
-          </div>
-        </section>
-        <section className="grid grid-cols-2 gap-8">
-          <div className="w-11/12 h-1/2">
-            <Image
-              src={props.notice.pageCover}
-              alt="noticePageCover"
-              width={9999}
-              height={9999}
-              layout="cover"
-            />
-          </div>
-          <div className="pt-12 w-3/4">
-            <h1 className="text-5xl font-extrabold">介绍</h1>
-            <p className="pt-6 pb-10 indent-8 text-xl"><b>这里写的是介绍里的文字 介绍里的文字 介绍里的文字 介绍里的文字 介绍里的文字 介绍里的文字介绍里的文字</b></p>
-            <p className="text-end">2023/07/20</p>
+            <div className="pt-12 w-3/4">
+              <h1 className="text-5xl font-extrabold">介绍</h1>
+              <p className="pt-6 pb-10 indent-8 text-xl"><b>这里写的是介绍里的文字 介绍里的文字 介绍里的文字
+                介绍里的文字 介绍里的文字 介绍里的文字介绍里的文字</b></p>
+              <p className="text-end">2023/07/20</p>
 
-          </div>
-        </section>
+            </div>
+          </section>
+          <section>
+            <div className="grid grid-cols-3 bg-slate-50 font-normal">
+              <div className="flex flex-col flex-wrap justify-around items-center border-inherit border">
+                <h1 className="text-5xl p-14 leading-snug font-normal">{props.posts[0].title}</h1>
+                <Image
+                    className="w-4/5 rounded-xl drop-shadow-2xl hover:shadow-2xl"
+                    src={props.posts[0].pageCover}
+                    alt="postImage"
+                    width={9999}
+                    height={9999}
+                    layout="cover"
+                />
+                <p className="m-2.5">
+                  <span className="text-l p-3calc rounded-md bg-blue leading-7 m-1">{props.tagOptions[0].name}</span>
+                  <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{props.tagOptions[1].name}</span>
+                  <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{props.tagOptions[2].name}</span>
+                </p>
+              </div>
+              <div className="flex flex-col flex-wrap justify-around items-center border-inherit border">
+                <h1 className="text-5xl p-14 leading-snug">{props.posts[1].title}</h1>
+                <Image
+                    className="w-4/5 rounded-xl drop-shadow-2xl hover:shadow-2xl"
+                    src={props.posts[1].pageCover}
+                    alt="postImage"
+                    width={9999}
+                    height={9999}
+                    layout="cover"
+                />
+                <p className="m-2.5">
+                  <span className="text-l p-3calc rounded-md bg-blue leading-7 m-1">{props.tagOptions[0].name}</span>
+                  <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{props.tagOptions[1].name}</span>
+                  <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{props.tagOptions[2].name}.</span>
+                </p>
+              </div>
+              <div className="flex flex-col flex-wrap justify-around items-center border-inherit border">
+                <h1 className="text-5xl p-14 leading-snug">{props.posts[2].title}</h1>
+                <Image
+                    className="w-4/5 rounded-xl drop-shadow-2xl hover:shadow-2xl"
+                    src={props.posts[2].pageCover}
+                    alt="postImage"
+                    width={9999}
+                    height={9999}
+                    layout="cover"
+                />
+                <p className="m-2.5">
+                  <span className="text-l p-3calc rounded-md bg-blue leading-7 m-1">{props.tagOptions[0].name}</span>
+                  <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{props.tagOptions[1].name}</span>
+                  <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{props.tagOptions[2].name}</span>
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
-    </div>
   );
 };
+
 // -------------------------------------------------------------------------//
 
 
@@ -90,11 +145,11 @@ const Index = (props) => {
  */
 export async function getStaticProps() {
   const from = "index";
-  const props = await getGlobalNotionData({ from });
+  const props = await getGlobalNotionData({from});
 
-  const { siteInfo } = props;
+  const {siteInfo} = props;
   props.posts = props.allPages.filter(
-    (page) => page.type === "Post" && page.status === "Published"
+      (page) => page.type === "Post" && page.status === "Published"
   );
 
   // 预览文章内容
@@ -105,9 +160,9 @@ export async function getStaticProps() {
         continue;
       }
       post.blockMap = await getPostBlocks(
-        post.id,
-        "slug",
-        BLOG.POST_PREVIEW_LINES
+          post.id,
+          "slug",
+          BLOG.POST_PREVIEW_LINES
       );
     }
   }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    // fontFamily: {
+    //   display: ['Source Serif Pro', 'Georgia', 'serif'],
+    //   body: ['Synonym', 'system-ui', 'sans-serif'],
+    // },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
