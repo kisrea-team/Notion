45a8f9d8619ea21f0dc12704ea219e017ccfcbd5
Vhko <hirsch-k@outlook.com>
2023-07-28 17:58:04 +0800

    🥲我也忘了写了什么

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import BLOG from "@/blog.config";
-import {getPostBlocks} from "@/lib/notion";
-import {getGlobalNotionData} from "@/lib/notion/getNotionData";
-import {setData} from "@/lib/notion/getPostBlocks";
+import { getPostBlocks } from "@/lib/notion";
+import { getGlobalNotionData } from "@/lib/notion/getNotionData";
+import { setData } from "@/lib/notion/getPostBlocks";
 //import Main from "./main"; //引入一个页面
 import Image from "next/image";
-import {Inter} from 'next/font/google'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] })
 // import { document } from "postcss";
 // import { useEffect, useState } from 'react'
-import React, {useEffect, useRef, useState} from "react";
-import anime from "animejs"
-import colors from "tailwindcss/colors";
+// import React, { useEffect, useRef, useState } from "react";
+// import anime from "animejs"
+// import colors from "tailwindcss/colors";
 // import Test from "./main";
 
 //import anime from 'animejs/lib/anime.es.js';//动画库 https://animejs.com/documentation/#cssSelector
@@ -21,25 +21,29 @@ import colors from "tailwindcss/colors";
 const Index = (props) => {
   const listItems = props.stars.map(product =>
 
-    <div>
-        <div className="flex flex-col flex-wrap justify-around items-center border-inherit border">
-          <h1 className="text-5xl p-14 leading-snug font-normal">{product.title}</h1>
-                    <Image
-                        className="w-4/5 rounded-xl drop-shadow-2xl hover:shadow-2xl"
-                        src={product.pageCover}
-                        alt="postImage"
-                        width={9999}
-                        height={9999}
-                        layout="cover"
-                    />
-                    <p className="m-2.5">
-                      <span className="text-l p-3calc rounded-md bg-blue leading-7 m-1">{product.tags[0]}</span>
-                      <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{product.tags[1]}</span>
-                      <span className="text-l p-3calc rounded-md bg-pink-100 leading-7 m-1">{product.tags[2]}</span>
-                    </p>
-        </div>
+    // eslint-disable-next-line react/jsx-key
+    <div className="flex flex-col flex-wrap items-center border-inherit border">
+      <h1 className="flex items-center flex-col text-5xl p-14 leading-snug font-normal">{product.title}
+        <span className=" text-lg text-slate-800">{product.date.start_date}</span>
+      </h1>
+      <Image
+        className="w-4/5 rounded-xl drop-shadow-2xl hover:shadow-2xl"
+        src={product.pageCover}
+        alt="postImage"
+        width={9999}
+        height={9999}
+        layout="cover"
+      />
+      <div className="mt-8 mx-14">
+        {product.tagItems.map((item, index) => (
+          <span key={index}
+            style={{ backgroundColor: item.color }}
+            className=" colored-texttext-l p-2calc rounded-md leading-8 m-1">
+            {item.name}
+          </span>))}
+      </div>
     </div>
-      );
+  );
   console.log(props)
   // const prop = JSON.stringify(props);
   // const listItems = props.posts.map((product) => (
@@ -52,61 +56,66 @@ const Index = (props) => {
   // };
   // ------------------------------------------------------------------------//
   return (
-      <div className="">
-        <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
-          <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{props.siteInfo.title}</h1>
-        </div>
-        <div
-            className="flex flex-wrap content-center justify-center w-full bg-white h-10 rounded-lg border-b border-gray-200">
-          <p className="text-sm">最新文章更新于{props.posts[0].createdTime}</p>
-        </div>
-        <div className="px-20 pb-16">
-          <section className=" w-full overflow-hidden grid grid-cols-1 grid-rows-2">
-            <div className="grid w-1/3 content-center">
-              <h1 className="text_vw_30 overflow-hidden text-clip font-medium ">{props.siteInfo.description}</h1>
-              <p className="flex justify-end flex-wrap pt-6 text_vw_15 text-left font-semibold">@{props.notice.Person[0].name} </p>
-            </div>
-            <div className="grid grid-cols-2 justify-items-end  content-center items-center h-full">
-              <div>
-              </div>
-              <div className="pt--1">
-                <Image
-                    className="width-auto"
-                    src={props.siteInfo.pageCover}
-                    alt="PageCover"
-                    width={9999}
-                    height={9999}
-                    layout="cover"
-                    objectFit="contain"
-                />
-              </div>
-            </div>
-          </section>
-          <section className="grid grid-cols-2 gap-8">
-            <div className="w-11/12 h-1/2">
-              <Image
-                  src={props.notice.pageCover}
-                  alt="noticePageCover"
-                  width={9999}
-                  height={9999}
-                  layout="cover"
-              />
-            </div>
-            <div className="pt-12 w-3/4">
-              <h1 className="text-5xl font-extrabold">介绍</h1>
-              <p className="pt-6 pb-10 indent-8 text-xl"><b>这里写的是介绍里的文字 介绍里的文字 介绍里的文字
-                介绍里的文字 介绍里的文字 介绍里的文字介绍里的文字</b></p>
-              <p className="text-end">2023/07/20</p>
-
-            </div>
-          </section>
-          <section>
-            <div className="grid grid-cols-3 bg-slate-50 font-normal">
-              {listItems}
-            </div>
-          </section>
-        </div>
+    <div className="container m-auto">
+      <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
+        <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{props.siteInfo.title}</h1>
       </div>
+      <div
+        className="flex flex-wrap content-center justify-center w-full bg-white h-10 rounded-lg border-b border-gray-200">
+        <p className="text-sm">最新文章更新于{props.posts[0].createdTime}</p>
+      </div>
+
+      <section className=" w-full overflow-hidden grid grid-cols-1 grid-rows-2">
+        <div className="grid w-1/3 content-center">
+          <h1 className="text_vw_30 sm:text-5xl overflow-hidden text-clip font-medium ">{props.siteInfo.description}</h1>
+          <p className="flex justify-end flex-wrap pt-6 text_vw_15 text-left font-semibold">@{props.notice.Person[0].name} </p>
+        </div>
+        <div className="grid grid-cols-2 justify-items-end  content-center items-center h-full">
+          <div>
+          </div>
+          <div className="pt--1">
+            <Image
+              className="width-auto"
+              src={props.siteInfo.pageCover}
+              alt="PageCover"
+              width={9999}
+              height={9999}
+              layout="cover"
+              objectFit="contain"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className="grid grid-cols-2 gap-8">
+        <div className="w-11/12 h-1/2">
+          <Image
+            src={props.notice.pageCover}
+            alt="noticePageCover"
+            width={9999}
+            height={9999}
+            layout="cover"
+          />
+        </div>
+        <div className="pt-12 w-3/4">
+          <h1 className="md:text-5xl sm:text-4xl  font-extrabold">介绍</h1>
+          <p className="pt-6 pb-10 indent-8 text-xl"><b>这里写的是介绍里的文字 介绍里的文字 介绍里的文字
+            介绍里的文字 介绍里的文字 介绍里的文字介绍里的文字</b></p>
+          <p className="text-end">2023/07/20</p>
+
+        </div>
+      </section>
+      <section>
+        <div className="grid grid-cols-3 bg-slate-50 font-normal">
+          {listItems}
+        </div>
+      </section>
+      <section>
+        <div>
+          <h1>{props.notice.summary}</h1>
+        </div>
+      </section>
+    </div>
   );
 };
 
@@ -119,11 +128,11 @@ const Index = (props) => {
  */
 export async function getStaticProps() {
   const from = "index";
-  const props = await getGlobalNotionData({from});
+  const props = await getGlobalNotionData({ from });
 
-  const {siteInfo} = props;
+  const { siteInfo } = props;
   props.posts = props.allPages.filter(
-      (page) => page.type === "Post" && page.status === "Published"
+    (page) => page.type === "Post" && page.status === "Published"
   );
 
   // 预览文章内容
@@ -134,9 +143,9 @@ export async function getStaticProps() {
         continue;
       }
       post.blockMap = await getPostBlocks(
-          post.id,
-          "slug",
-          BLOG.POST_PREVIEW_LINES
+        post.id,
+        "slug",
+        BLOG.POST_PREVIEW_LINES
       );
     }
   }
