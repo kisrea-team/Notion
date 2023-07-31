16c5c42de9b099c2865876a35bdaee7e8b429eee
Vhko <hirsch-k@outlook.com>
2023-07-31 11:26:06 +0800

    布局

--- pages/index.js
+++ pages/index.js
@@ -24,27 +24,27 @@ const Index = (props) => {
     // eslint-disable-next-line react/jsx-key
     <div>
       <a href={product.id}>
-      <div className="flex flex-col flex-wrap items-center border-inherit border">
-        <h1 className="flex items-center flex-col text-5xl p-14 leading-snug font-normal">{product.title}
-          <span className=" text-lg text-slate-800">{product.date.start_date}</span>
-        </h1>
-        <Image
-          className="w-4/5 rounded-xl drop-shadow-2xl hover:shadow-2xl"
-          src={product.pageCover}
-          alt="postImage"
-          width={9999}
-          height={9999}
-          layout="cover"
-        />
-        <div className="mt-8 mx-14">
-          {product.tagItems.map((item, index) => (
-            <span key={index}
-              style={{ backgroundColor: item.color }}
-              className=" colored-texttext-l p-2calc rounded-md leading-8 m-1">
-              {item.name}
-            </span>))}
+        <div className="flex flex-col flex-wrap items-center border-inherit border">
+          <h1 className="flex items-center flex-col text-5xl p-14 leading-snug font-normal">{product.title}
+            <span className=" text-lg text-slate-800">{product.date.start_date}</span>
+          </h1>
+          <Image
+            className="w-4/5 rounded-xl drop-shadow-2xl hover:shadow-2xl"
+            src={product.pageCover}
+            alt="postImage"
+            width={9999}
+            height={9999}
+            layout="cover"
+          />
+          <div className="mt-8 mx-14">
+            {product.tagItems.map((item, index) => (
+              <span key={index}
+                style={{ backgroundColor: item.color }}
+                className=" colored-texttext-l p-2calc rounded-md leading-8 m-1">
+                {item.name}
+              </span>))}
+          </div>
         </div>
-      </div>
       </a>
     </div>
   );
@@ -60,7 +60,7 @@ const Index = (props) => {
   // };
   // ------------------------------------------------------------------------//
   return (
-    <div className="container m-auto">
+    <div>
       <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
         <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{props.siteInfo.title}</h1>
       </div>
@@ -68,57 +68,56 @@ const Index = (props) => {
         className="flex flex-wrap content-center justify-center w-full bg-white h-10 rounded-lg border-b border-gray-200">
         <p className="text-sm">最新文章更新于{props.posts[0].createdTime}</p>
       </div>
+      <div className="container m-auto">
 
-      <section className=" w-full overflow-hidden grid grid-cols-1 grid-rows-2">
-        <div className="grid w-1/3 content-center">
-          <h1 className="text_vw_30 sm:text-5xl overflow-hidden text-clip font-medium ">{props.siteInfo.description}</h1>
-          <p className="flex justify-end flex-wrap pt-6 text_vw_15 text-left font-semibold">@{props.notice.Person[0].name} </p>
-        </div>
-        <div className="grid grid-cols-2 justify-items-end  content-center items-center h-full">
-          <div>
+        <section className=" w-full overflow-hidden grid grid-cols-2 grid-row-121 relative">
+          <div className="grid ">
+            <h1 className=" text_vw_30 sm:text-5xl overflow-hidden text-clip font-medium bg-red-800">{props.siteInfo.description}</h1>
+            <p className="flex justify-end flex-wrap pt-6 text_vw_15 text-left font-semibold">@{props.notice.Person[0].name} </p>
           </div>
-          <div className="pt--1">
+          <div className="row-2">
             <Image
-              className="width-auto"
+              className="justify-items-end content-end h-full"
               src={props.siteInfo.pageCover}
               alt="PageCover"
               width={9999}
               height={9999}
               layout="cover"
-              objectFit="contain"
+            // objectFit="contain"
             />
           </div>
-        </div>
-      </section>
 
-      <section className="grid grid-cols-2 gap-8">
-        <div className="w-11/12 h-1/2">
-          <Image
-            src={props.notice.pageCover}
-            alt="noticePageCover"
-            width={9999}
-            height={9999}
-            layout="cover"
-          />
-        </div>
-        <div className="pt-12 w-3/4">
-          <h1 className="md:text-5xl sm:text-4xl  font-extrabold">介绍</h1>
-          <p className="pt-6 pb-10 indent-8 text-xl"><b>这里写的是介绍里的文字 介绍里的文字 介绍里的文字
-            介绍里的文字 介绍里的文字 介绍里的文字介绍里的文字</b></p>
-          <p className="text-end">2023/07/20</p>
+        </section>
 
-        </div>
-      </section>
-      <section>
-        <div className="grid grid-cols-3 bg-slate-50 font-normal">
-          {listItems}
-        </div>
-      </section>
-      <section>
-        <div>
-          <h1>{props.notice.summary}</h1>
-        </div>
-      </section>
+        <section className="grid grid-cols-2 gap-8">
+          <div className="w-11/12 h-1/2">
+            <Image
+              src={props.notice.pageCover}
+              alt="noticePageCover"
+              width={9999}
+              height={9999}
+              layout="cover"
+            />
+          </div>
+          <div className="pt-12 w-3/4">
+            <h1 className="md:text-5xl sm:text-4xl  font-extrabold">介绍</h1>
+            <p className="pt-6 pb-10 indent-8 text-xl"><b>这里写的是介绍里的文字 介绍里的文字 介绍里的文字
+              介绍里的文字 介绍里的文字 介绍里的文字介绍里的文字</b></p>
+            <p className="text-end">2023/07/20</p>
+
+          </div>
+        </section>
+        <section>
+          <div className="grid grid-cols-3 bg-slate-50 font-normal">
+            {listItems}
+          </div>
+        </section>
+        <section>
+          <div>
+            <h1>{props.notice.summary}</h1>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
