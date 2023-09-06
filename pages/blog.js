5b07aa257856eb237c0f722cfb0307771ae8f3b7
vhko <hirsch-k@outlook.com>
2023-09-06 19:08:43 +0800

    添加MUI框架

--- pages/blog.js
+++ pages/blog.js
@@ -4,66 +4,69 @@ import { getData } from '@/lib/notion/getPostBlocks';
 // import { getDatabase } from '@/lib/notion/getDatabase';
 // import { getCollectionData } from '@/lib/notion/getCollectionData'; 
 import Image from 'next/image';
+import * as React from 'react';
 import { useState } from 'react';
 // import { list } from 'postcss';
 // @param {*} props
 // @returns
+import { Button, IconButton } from '@mui/material';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 const style = {
-    img: {
-        overflow: 'clip',
-        overflowClipMargin: 'content-box',
-        transform: 'Scale(105%)'
-    }
+   img: {
+      overflow: 'clip',
+      overflowClipMargin: 'content-box',
+      transform: 'Scale(105%)'
+   }
 }
 const Test = data => {
-    console.log(data)
-    return (
-        <main className=''>
-            <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
-                <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{data.data.siteInfo.title}</h1>
+   console.log(data)
+   return (
+      <main className=''>
+         <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
+            <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{data.data.siteInfo.title}</h1>
+            <IconButton aria-label='Brightness4'><Brightness4Icon /></IconButton>
+         </div>
+         <section>
+            <div className='container mx-auto flex'>
+               {/* ---------------------- */}
+               <div className=' flex border-2 border-solid'>
+                  <div className='flex image-container items-center w-350 h-200'>
+                     <Image
+                        style={style.img}
+                        className='static blur-1'
+                        src={data.data.posts[0].pageCover}
+                        alt="noticePageCover"
+                        width={400}
+                        height={300}
+                     />
+                  </div>
+                  <span></span>
+                  <div>
+                     <div><h1 className='font-size1'>{data.data.posts[0].title}</h1></div>
+                     <div><p className='size-vh-100'>{data.data.posts[0].date.start_date}</p>
+                        <p className='md:text-2xl xl:text-lg text-gray-700'>{data.data.posts[0].summary}</p></div>
+                  </div>
+               </div>
             </div>
-            <section>
-                <div className='container mx-auto flex '>
-                    <div className=' flex items-center border-2 border-solid'>
-                        <div className='flex image-container items-center w-350 h-200'>
-                            <Image
-                                style={style.img}
-                                className='static blur-1'
-                                src={data.data.posts[0].pageCover}
-                                alt="noticePageCover"
-                                width={400}
-                                height={300}
-                            />
-                        </div>
-                        <h1 className='font-size1'>{data.data.posts[0].title}</h1>
-                        <p className='size-vh-100'>{data.data.posts[0].date.start_date}</p>
-                        <p>{data.data.posts[0].summary}</p>
-                        {/* <div>
-                            <h1 className='font-size1'>{data.data.posts[0].title}</h1>
-                            <p className='size-vh-100'>{data.data.posts[0].date.start_date}</p>
-                            <p>{data.data.posts[0].summary}</p>
-                        </div> */}
-                    </div>
-                </div>
 
-            </section>
-        </main>
-    )
+         </section>
+      </main>
+   )
 }
 /**
  * SSG 获取数据
  * @returns
  */
 export async function getStaticProps() {
-    const data = await getData()
-    if (!data) {
-        return {
-            notFound: true,
-        }
-    }
-    return {
-        props: { data }, // will be passed to the page component as props
-    }
+   const data = await getData()
+   if (!data) {
+      return {
+         notFound: true,
+      }
+   }
+   return {
+      props: { data }, // will be passed to the page component as props
+   }
 }
 
 export default Test
