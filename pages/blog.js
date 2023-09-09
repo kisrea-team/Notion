a5500b0ec82feca88c56b18b161a046cf69f47ec
vhko <hirsch-k@outlook.com>
2023-09-09 11:55:46 +0800

    修饰Gard 改为列表形式

--- pages/blog.js
+++ pages/blog.js
@@ -9,71 +9,66 @@ import { useState } from 'react';
 // import { list } from 'postcss';
 // @param {*} props
 // @returns
-import { Button, IconButton } from '@mui/material';
+import { Button, Card, CardMedia, IconButton, CardContent, Typography, Avatar } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
+import { productionBrowserSourceMaps } from '@/next.config';
 const style = {
    img: {
       overflow: 'clip',
       overflowClipMargin: 'content-box',
       transform: 'Scale(105%)'
+   },
+   item: {
+      fontSize: '4vh',
+      marginRight: '5vh',
    }
 }
-// const abc = data.data.posts[0].date.start_date
-// const editedDate = new Date(abc);
-// const currentDate = new Date();
-// const timeDifference = currentDate - editedDate;
-// const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-// console.log(`距离最后编辑时间已经过去了 ${daysDifference} 天`);
-// console.log(abc)
 const Test = data => {
+   const listItems = data.data.posts.map(product =>
+      // eslint-disable-next-line react/jsx-key
+      <section>
+         <div className='container mx-auto flex m-10'>
+            <Card sx={{ borderRadius: 2, }} className=' gard-width gard-display flex' raised='true' >
+               <div className='flex image-container items-center card-img'>
+                  <CardMedia
+                     className='static blur-1 '
+                     component="img"
+                     alt="green iguana"
+                     height="160"
+                     src={product.pageCover} />
+               </div>
+               <CardContent className='card-text'>
+                  <Typography gutterBottom variant="h5" component="div">
+                     {product.title}
+                  </Typography>
+                  <Typography>{product.lastEditedTime}天前</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                     {product.summary}
+                  </Typography>
+               </CardContent>
+            </Card>
+         </div>
+      </section>
+   );
    console.log(data)
    return (
       <main className=''>
-         <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
-            <h1 className="2xl:text-xl sm:text-sm font-semibold text-justify ml-10">{data.data.siteInfo.title}</h1>
-            <IconButton aria-label='Brightness4'><Brightness4Icon /></IconButton>
-         </div>
-         <section>
-            <div className='container mx-auto flex'>
-               {/* ---------------------- */}
-               <div className=' flex border-2 border-solid'>
-                  <div className='flex image-container items-center w-350 h-200'>
-                     <Image
-                        style={style.img}
-                        className='static blur-1'
-                        src={data.data.posts[0].pageCover}
-                        alt="noticePageCover"
-                        width={400}
-                        height={300}
-                     />
-                  </div>
-                  <span></span>
-                  <div>
-                     <div><h1 className='font-size1'>{data.data.posts[0].title}</h1></div>
-                     <div><p className='size-vh-100'>{data.data.posts[0].lastEditedTime}天前</p>
-                        <p className='md:text-2xl xl:text-lg text-gray-700'>{data.data.posts[0].summary}</p></div>
-                  </div>
-               </div>
-               <div className=' flex border-2 border-solid'>
-                  <div className='flex image-container items-center w-350 h-200'>
-                     <Image
-                        style={style.img}
-                        className='static blur-1'
-                        src={data.data.posts[1].pageCover}
-                        alt="noticePageCover"
-                        width={400}
-                        height={300}
-                     />
-                  </div>
-                  <span></span>
-                  <div>
-                     <div><h1 className='font-size1'>{data.data.posts[1].title}</h1></div>
-                     <div><p className='size-vh-100'>{data.data.posts[1].lastEditedTime}天前</p>
-                        <p className='md:text-2xl xl:text-lg text-gray-700'>{data.data.posts[1].summary}</p></div>
-                  </div>
-               </div>
-            </div>
 
+         <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
+            <span className="2xl:text-2xl sm:text-xl font-semibold text-justify ml-10">{data.data.siteInfo.title}</span>
+            <div className=' flex text-justify mr-10'>
+               <p style={style.item}>最新文章</p>
+               <p style={style.item}>关于</p>
+            </div>
+            <IconButton aria-label='Brightness4'><Brightness4Icon /></IconButton>
+            <Avatar></Avatar>
+         </div>
+
+         <section>
+
+         </section>
+         <section>
+            {listItems}
          </section>
       </main>
    )
