aee09d35ea8a24fd08d4038a95ee9d34b72e8382
vhko <hirsch-k@outlook.com>
2023-09-24 11:53:33 +0800

    修改了一些样式安装今日诗词API

--- pages/blog.js
+++ pages/blog.js
@@ -9,7 +9,7 @@ import { useState } from 'react';
 // import { list } from 'postcss';
 // @param {*} props
 // @returns
-import { Button, Card, CardMedia, IconButton, CardContent, Typography, Avatar } from '@mui/material';
+import { Button, Card, CardMedia, IconButton, CardContent, Typography, Avatar, colors } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 // import { productionBrowserSourceMaps } from '@/next.config';
 const style = {
@@ -21,39 +21,46 @@ const style = {
    item: {
       fontSize: '2vh',
       marginRight: '5vh',
-   }
+      color: '#D6DBDC'
+   },
 }
+const jinrishici = require('jinrishici');
+jinrishici.load(shici => {
+   console.log(shici);
+});
 const Test = data => {
    const listItems = data.data.posts.map(product =>
       // eslint-disable-next-line react/jsx-key
       <section>
-         <div className='container mx-auto flex m-10'>
-            <Card sx={{ borderRadius: 2, }} className=' gard-width gard-display flex' raised='true' >
-               <div className='flex image-container items-center card-img'>
-                  <CardMedia
-                     className='static blur-1 '
-                     component="img"
-                     alt="green iguana"
-                     height="160"
-                     src={product.pageCover} />
-               </div>
-               <CardContent className='card-text'>
-                  <Typography gutterBottom variant="h5" component="div">
-                     {product.title}
-                  </Typography>
-                  <Typography>{product.lastEditedTime}天前</Typography>
-                  <Typography variant="body2" color="text.secondary">
-                     {product.summary}
-                  </Typography>
-               </CardContent>
-            </Card>
+         <div className='container mx-auto flex'>
+            <a href={product.id}>
+               <Card sx={{ borderRadius: 2, }} className=' card-width card-height card-display flex' >
+                  <div className='flex image-container items-center card-img '>
+                     <CardMedia
+                        className='static blur-1 '
+                        component="img"
+                        alt="green iguana"
+                        height="160"
+                        src={product.pageCover} />
+                  </div>
+                  <CardContent className='card-text'>
+                     <Typography gutterBottom variant="h5" component="div">
+                        {product.title}
+                     </Typography>
+                     <Typography>{product.lastEditedTime}天前</Typography>
+                     {/* <Typography variant="body2" color="text.secondary" className='overflow-hidden text-clip'>
+                        {product.summary}
+                     </Typography> */}
+                     <p className='text-clip'>{product.summary}</p>
+                  </CardContent>
+               </Card>
+            </a>
          </div>
       </section>
    );
    console.log(data)
    return (
-      <main className=''>
-
+      <main className='bg-main'>
          <div className="flex h-16 bg-red-200 flex-wrap content-center md:box-content">
             <span className="2xl:text-2xl sm:text-xl font-semibold text-justify ml-10">{data.data.siteInfo.title}</span>
             <div className=' flex text-justify mr-10'>
@@ -63,13 +70,13 @@ const Test = data => {
             <IconButton aria-label='Brightness4'><Brightness4Icon /></IconButton>
             <Avatar></Avatar>
          </div>
-
-         <section>
-
-         </section>
-         <section>
+         <div>
+            <h1 className=" text_vw_30 sm:text-5xl overflow-hidden text-clip font-medium">{data.data.siteInfo.description}</h1>
+            <p className="flex justify-end flex-wrap pt-6 text_vw_15 text-left font-semibold">@{data.data.notice.Person[0].name} </p>
+         </div>
+         <div className='list space-y-[20px]'>
             {listItems}
-         </section>
+         </div>
       </main>
    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    // fontFamily: {
-    //   display: ['Source Serif Pro', 'Georgia', 'serif'],
-    //   body: ['Synonym', 'system-ui', 'sans-serif'],
-    // },
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+   content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+   ],
+   theme: {
+      extend: {
+         backgroundImage: {
+            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+            'gradient-conic':
+               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 }
